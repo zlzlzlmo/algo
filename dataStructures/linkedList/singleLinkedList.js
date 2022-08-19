33b429226761810dbbe2cbd064d8b9c2adfeca15
신승훈 <zlzlzlmo@daum.net>
2022-08-20 08:03:11 +0900

    linked list pop

--- dataStructures/linkedList/singleLinkedList.js
+++ dataStructures/linkedList/singleLinkedList.js
@@ -14,7 +14,7 @@ class SingleLinkedList {
 
   push(val) {
     const newNode = new Node(val);
-    if (!this.head || !this.tail) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
       this.length += 1;
@@ -25,13 +25,35 @@ class SingleLinkedList {
     this.tail = newNode;
     this.length += 1;
   }
+
+  pop() {
+    if (!this.head) return;
+
+    let currentNode = this.head;
+    let prevNode = this.head;
+
+    while (currentNode.next) {
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+    }
+
+    this.tail = prevNode;
+    this.tail.next = null;
+    this.length -= 1;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+  }
 }
 
 const linkedList = new SingleLinkedList();
 linkedList.push(3);
 linkedList.push(5);
 linkedList.push(15);
+linkedList.pop();
+linkedList.pop();
+linkedList.pop();
 
-console.log(linkedList.head);
-console.log(linkedList.head.next);
-console.log(linkedList.head.next.next);
+console.log(linkedList);
