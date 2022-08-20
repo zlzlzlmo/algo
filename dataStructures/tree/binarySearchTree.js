af9ac207d33ccd075d3b919f53f25b0c7d929c58
신승훈 <zlzlzlmo@daum.net>
2022-08-20 18:05:33 +0900

    binarysearch tree find

--- dataStructures/tree/binarySearchTree.js
+++ dataStructures/tree/binarySearchTree.js
@@ -37,12 +37,32 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(val) {
+    if (!this.root) return false;
+    if (this.root.val === val) return true;
+    let current = this.root;
+    while (current) {
+      if (current.val === val) return true;
+
+      if (val < current.val) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
 tree.insert(3);
-tree.insert(3);
-tree.insert(3);
-tree.insert(3);
+tree.insert(10);
+tree.insert(4);
+tree.insert(12);
+tree.insert(110);
+tree.insert(150);
+tree.insert(19);
 
-console.log(tree);
+console.log(tree.find(150));
