ad6473ebede6f8ceb280822f69b552c257a95007
신승훈 <zlzlzlmo@daum.net>
2022-08-21 08:13:36 +0900

    maxbinaryheap extract max

--- dataStructures/heap/maxBinaryHeap.js
+++ dataStructures/heap/maxBinaryHeap.js
@@ -30,6 +30,46 @@ class MaxBinaryHeap {
     this.values[firstIdx] = second;
     this.values[secondIdx] = first;
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const popped = this.values.pop();
+    this.values[0] = popped;
+
+    this.sinkDown();
+    return max;
+  }
+
+  sinkDown() {
+    let currentIdx = 0;
+    while (true) {
+      let leftChildIdx = 2 * currentIdx + 1;
+      let rightChildIdx = 2 * currentIdx + 2;
+      const currentElement = this.values[currentIdx];
+      const leftChildElement = this.values[leftChildIdx];
+      const rightChildElement = this.values[rightChildIdx];
+      let swapIdx = null;
+
+      if (leftChildIdx < this.values.length) {
+        if (leftChildElement > currentElement) {
+          swapIdx = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < this.values.length) {
+        if (
+          (!swapIdx && rightChildElement > currentElement) ||
+          (swapIdx && rightChildElement > leftChildElement)
+        ) {
+          swapIdx = rightChildIdx;
+        }
+      }
+
+      if (!swapIdx) break;
+      this.swap(currentIdx, swapIdx);
+      currentIdx = swapIdx;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
@@ -40,5 +80,8 @@ heap.insert(24);
 heap.insert(41);
 heap.insert(222);
 heap.insert(1412321);
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
 
 console.log(heap.values);
