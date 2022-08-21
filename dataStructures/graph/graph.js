d7e26126a74de6d41cf225022451c6958c6db932
신승훈 <zlzlzlmo@daum.net>
2022-08-21 15:26:52 +0900

    graph remove edge

--- dataStructures/graph/graph.js
+++ dataStructures/graph/graph.js
@@ -13,6 +13,20 @@ class Graph {
     this.adjacencyList[vertex].push(vertex2);
     this.adjacencyList[vertex2].push(vertex);
   }
+
+  removeEdge(vertex, vertex2) {
+    if (
+      !this.adjacencyList[vertex] ||
+      !this.adjacencyList[vertex].includes(vertex2)
+    )
+      return;
+
+    const idx1 = this.adjacencyList[vertex].findIndex((v) => v === vertex2);
+    const idx2 = this.adjacencyList[vertex2].findIndex((v) => v === vertex);
+
+    this.adjacencyList[vertex].splice(idx1, 1);
+    this.adjacencyList[vertex2].splice(idx2, 1);
+  }
 }
 
 const graph = new Graph();
@@ -22,4 +36,5 @@ graph.addVertex("Dallas");
 graph.addVertex("Aspen");
 graph.addEdge("Tokyo", "Dallas");
 graph.addEdge("Dallas", "Aspen");
+graph.removeEdge("Dallas", "Aspen");
 console.log(graph.adjacencyList);
