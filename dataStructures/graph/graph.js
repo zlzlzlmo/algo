9f0b11f951bcb64d14eae968f522b51afa8ee2d4
신승훈 <zlzlzlmo@daum.net>
2022-08-21 15:19:25 +0900

    graph add Edge

--- dataStructures/graph/graph.js
+++ dataStructures/graph/graph.js
@@ -7,11 +7,19 @@ class Graph {
     if (this.adjacencyList[vertex]) return;
     this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex, vertex2) {
+    if (!this.adjacencyList[vertex] || !this.adjacencyList[vertex2]) return;
+    this.adjacencyList[vertex].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex);
+  }
 }
 
 const graph = new Graph();
 
-graph.addVertex("TEST");
-graph.addVertex("TEST1");
-graph.addVertex("TEST");
+graph.addVertex("Tokyo");
+graph.addVertex("Dallas");
+graph.addVertex("Aspen");
+graph.addEdge("Tokyo", "Dallas");
+graph.addEdge("Dallas", "Aspen");
 console.log(graph.adjacencyList);
