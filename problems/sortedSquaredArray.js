1800a57c0a8b17eb54e465c214f49887ca1c188c
신승훈 <zlzlzlmo@daum.net>
2022-08-26 22:27:08 +0900

    nodd depths

--- problems/sortedSquaredArray.js
+++ problems/sortedSquaredArray.js
@@ -1,7 +1,18 @@
-function sortedSquaredArray(array) {
-  const result = [];
-  for (const element of array) {
-    result.push(element ** 2);
+function nodeDepths(root) {
+  return sumDepths(root, 0);
+}
+
+function sumDepths(node, level) {
+  if (!node) return 0;
+  return (
+    level + sumDepths(node.left, level + 1) + sumDepths(node.legt, level + 1)
+  );
+}
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  return result.sort((a, b) => a - b);
 }
