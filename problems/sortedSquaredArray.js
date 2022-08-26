3938df71527330e80827836b48548eb95b0c74de
신승훈 <zlzlzlmo@daum.net>
2022-08-27 07:05:55 +0900

    re try sortedSquaredArray

--- problems/sortedSquaredArray.js
+++ problems/sortedSquaredArray.js
@@ -1,12 +1,14 @@
 function nodeDepths(root) {
-  return sumDepths(root, 0);
-}
-
-function sumDepths(node, level) {
-  if (!node) return 0;
-  return (
-    level + sumDepths(node.left, level + 1) + sumDepths(node.legt, level + 1)
-  );
+  // left 혹은 right의 노드가 있으면 계속 파고들어 depth + 1을한다
+  // 실행이 끝나면 result를 리턴한다
+  let result = 0;
+  traverse(root, 0);
+  function traverse(node, currentDepth) {
+    result += currentDepth;
+    if (node.left) traverse(node.left, currentDepth + 1);
+    if (node.right) traverse(node.right, currentDepth + 1);
+  }
+  return result;
 }
 
 class BinaryTree {
