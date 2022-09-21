1f6bb48201886a698688dd1e55647303edb2dac4
신승훈 <zlzlzlmo@daum.net>
2022-09-21 23:07:31 +0900

    moveElementToEnd

---
+++ problems/moveElementToEnd.js
@@ -0,0 +1,17 @@
+function moveElementToEnd(array, toMove) {
+  const firstArray = [];
+  const secondArray = [];
+
+  array.forEach((element) => {
+    if (element === toMove) {
+      secondArray.push(element);
+    } else {
+      firstArray.push(element);
+    }
+  });
+
+  // 2 1 2 2 2 3 4 2
+  // 1 2 2 2 2 3 4 2
+  // 타겟이2 이면 맨뒤로 보내면된다..
+  return [...firstArray, ...secondArray];
+}
