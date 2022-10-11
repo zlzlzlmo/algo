8c5b5779b22aae0135e362eafcb73e84d57a39c5
신승훈 <zlzlzlmo@daum.net>
2022-10-11 22:00:52 +0900

    getLogestPalidrome

--- problems/longestPalindromicSubstring.js
+++ problems/longestPalindromicSubstring.js
@@ -1,6 +1,13 @@
 function longestPalindromicSubstring(string) {
-  // Write your code here.
+  let currentLongest = [0, 1];
+  for (let i = 0; i < string.length; i += 1) {
+    const odd = getLogestPalidrome(string, i - 1, i + 1);
+    const event = getLogestPalidrome(string, i - 1, i);
+  }
 }
 
-// Do not edit the line below.
-exports.longestPalindromicSubstring = longestPalindromicSubstring;
+function getLogestPalidrome(string, leftIdx, rightIdx) {
+  while (leftIdx >= 0 && rightIdx < string.length) {
+    // if(string)
+  }
+}
