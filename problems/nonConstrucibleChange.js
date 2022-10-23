ccb96dff7f2f97f88c3e8cf82ccc58e077592985
신승훈 <zlzlzlmo@daum.net>
2022-10-23 22:11:41 +0900

    nonCon

--- problems/nonConstrucibleChange.js
+++ problems/nonConstrucibleChange.js
@@ -8,7 +8,6 @@ function nonConstructibleChange(coins) {
 
   for (const coin of coins) {
     if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
-
     currentChangeCreated += coin;
   }
   return currentChangeCreated + 1;
